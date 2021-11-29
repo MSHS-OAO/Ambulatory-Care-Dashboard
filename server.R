525484587c3e96eb20410af5117a227953b2f836
Villegas <armando.villegas@mssm.edu>
2021-11-29 16:54:54 -0500

    update y aaxis title

--- server.R
+++ server.R
@@ -4901,7 +4901,7 @@ server <- function(input, output, session) {
     graph <- ggplot(space.hour, aes(x=Time, y=value, col=variable, group=variable))+
       geom_line(size=1.2)+
       scale_y_continuous(labels = scales::percent_format(accuracy = 1), limits = c(0,max(space.hour$value)*1.2))+
-      labs(x=NULL, y="Number of Rooms\n", 
+      labs(x=NULL, y="Percent", 
            title = "Space Utilization (%) by Percentile by Time of Day",
            subtitle = paste0("Based on scheduled appointment time and duration from ",isolate(input$dateRangeUtil[1])," to ",isolate(input$dateRangeUtil[2])))+
       scale_color_MountSinai("main")+
