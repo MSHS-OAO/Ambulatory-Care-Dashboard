a976c89f66ab944525a3832e653805fa001f3f69
aghaer01 <roya.aghaeifar@mssm.edu>
2025-01-02 13:25:03 -0500

    Changed amb access summary to production

--- global.R
+++ global.R
@@ -402,7 +402,8 @@ utilization.data <- utilization.data %>% rename(`07:00`= "H_07_00", `08:00`= "H_
 population_tbl <- tbl(poolcon_production, "AMBULATORY_POPULATION") %>% filter(APPT_STATUS == "Arrived")
 print("Population")
 
-ambulatory_access_tbl_summary <- tbl(poolcon_upt, "AMBULATORY_ACCESS_SUMMARY_TABLE")
+#ambulatory_access_tbl_summary <- tbl(poolcon_upt, "AMBULATORY_ACCESS_SUMMARY_TABLE")
+ambulatory_access_tbl_summary <- tbl(poolcon_production, "AMBULATORY_ACCESS_SUMMARY_TABLE")
 print("summary")
 ambulatory_access_tbl_summary_npr <- tbl(poolcon_upt, "AMBULATORY_ACCESS_NPR_SUMMARY_TABLE")
 print("npr")
