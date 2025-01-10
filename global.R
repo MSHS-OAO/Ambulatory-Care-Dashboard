d29935dc6ae76f036a707beacbd6acd07d2ca47c
aghaer01 <roya.aghaeifar@mssm.edu>
2025-01-10 15:20:21 -0500

    updated AM-PM

--- global.R
+++ global.R
@@ -405,7 +405,7 @@ print("Population")
 #ambulatory_access_tbl_summary <- tbl(poolcon_upt, "AMBULATORY_ACCESS_SUMMARY_TABLE")
 ambulatory_access_tbl_summary <- tbl(poolcon_production, "AMBULATORY_ACCESS_SUMMARY_TABLE")
 print("summary")
-ambulatory_access_tbl_summary_npr <- tbl(poolcon_upt, "AMBULATORY_ACCESS_NPR_SUMMARY_TABLE")
+ambulatory_access_tbl_summary_npr <- tbl(poolcon_production, "AMBULATORY_ACCESS_NPR_SUMMARY_TABLE")
 print("npr")
 
 
